3253f99acd1f6087789acdd997f7c04828a24183
DiyaMathew02 <diamathew02@gmail.com>
2025-10-27 02:25:55 +0000

    python packets for projects

---
+++ models/train_ann.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+from scikitlearn.model_selection import train_test_split
+from tensorflow.keras.models import StandardScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.models import load_model
+
